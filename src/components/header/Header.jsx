d47770471cd6412b33af8dda501f52470cfde20f
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2022-11-16 20:12:30 +0200

    wtf

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,5 @@
 // Imports
 import styles from './header.module.scss';
-import Button from '../../elements/button/Button';
-import Link from '../../elements/link/Link';
-import { ButtonTypes } from '../../types/Button';
 import Logo from '../../assets/images/LOGO.png';
 
 // Header
@@ -11,14 +8,8 @@ const Header = () => {
     <header className={styles.header}>
       {/* Logo */}
       <div className={styles.logo_container}>
-        <div className={styles.header__button}>
-          <Button text="Reservation" type={ButtonTypes.GOLD} />
-        </div>
         <div className={styles.header__logo}>
           <img src={Logo} alt="logo" />
-        </div>
-        <div className={styles.header__button}>
-          <Link text="+380984956069" href="tel:+380984956069" />
         </div>
       </div>
       {/* Line */}
--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -1,12 +1,10 @@
 import styles from './navigation.module.scss';
 import Link from '../../elements/link/Link';
 import Instagram from '../../assets/images/instagram.png';
-import Facebook from '../../assets/images/facebook.png';
-import Twitter from '../../assets/images/twitter.png';
-import Pinterest from '../../assets/images/pinterest.png';
 import { useEffect } from 'react';
 import './nav.css';
 import Burger from './Burger';
+import { LinkTypes } from 'types/Link';
 // import { slide as Menu } from 'react-burger-menu';
 
 const Navigation = () => {
@@ -28,7 +26,7 @@ const Navigation = () => {
   };
 
   return (
-    <section className='header-section'>
+    <section className="header-section">
       <div className={styles.container}>
         <div className={styles.nav_block}>
           {/* Nav */}
@@ -38,21 +36,18 @@ const Navigation = () => {
             <Link text="Our Menu" href="#sec-3" />
             <Link text="Pages" href="#sec-4" />
             <Link text="Blog" href="#sec-5" />
-            <Link text="Contact Us" href="#sec-6" />
           </div>
           {/* Icons */}
           <div className={styles.icons}>
             <div className={styles.icon}>
               <img src={Instagram} alt="intagramm" />
             </div>
-            <div className={styles.icon}>
-              <img src={Facebook} alt="facebook" />
-            </div>
-            <div className={styles.icon}>
-              <img src={Twitter} alt="twitter" />
-            </div>
-            <div className={styles.icon}>
-              <img src={Pinterest} alt="pinterest" />
+            <div className={styles.numbers}>
+              <Link
+                text="+380984956069"
+                href="tel:+380984956069"
+                type={LinkTypes.WHITE}
+              />
             </div>
           </div>
           {/* Burger */}
