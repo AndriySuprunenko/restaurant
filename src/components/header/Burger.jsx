7032d4c5ce63f52ebcd6f42d24d41fa487acf6e5
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2022-11-16 22:46:41 +0200

    burger

--- src/components/header/Burger.jsx
+++ src/components/header/Burger.jsx
@@ -6,9 +6,15 @@ import './nav.css';
 const Burger = () => {
   useEffect(() => {
     const hamb = document.querySelector('#hamb');
+    const popup = document.querySelector('#popup');
     hamb.addEventListener('click', open);
+    popup.addEventListener('click', open);
+    return () => {
+      hamb.removeEventListener('click', open);
+      popup.removeEventListener('click', open);
+    };
   });
-  
+
   const open = (e) => {
     const popup = document.querySelector('#popup');
     const hamb = document.querySelector('#hamb');
@@ -27,23 +33,20 @@ const Burger = () => {
       </div>
       <div className="popup" id="popup">
         <div className={styles.menu__link}>
-          <div className={styles.link} id="link">
-            <Link text="Home" href="#sec-1" />
+          <div className={styles.link}>
+            <Link text="Home" href="#sec-2" />
           </div>
-          <div className={styles.link} id="link">
+          <div className={styles.link}>
             <Link text="Aboute Us" href="#sec-2" />
           </div>
-          <div className={styles.link} id="link">
+          <div className={styles.link}>
             <Link text="Our Menu" href="#sec-3" />
           </div>
-          <div className={styles.link} id="link">
+          <div className={styles.link}>
             <Link text="Pages" href="#sec-4" />
           </div>
-          <div className={styles.link} id="link">
+          <div className={styles.link}>
             <Link text="Blog" href="#sec-5" />
-          </div>
-          <div className={styles.link} id="link">
-            <Link text="Contact Us" href="#sec-6" />
           </div>
         </div>
       </div>
